f5178526bf8bcdddfec05784b0c8448be3b78d2c
Keisha C <chingk20@up.edu>
2019-10-07 17:31:06 -0700

    initial commit

---
+++ app/src/main/java/com/example/gamestate/GameState.java
@@ -0,0 +1,104 @@
+package com.example.gamestate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Random;
+
+public class GameState {
+    private int playerTurn; //which players turn it is
+    private int bidNum; //amount player chooses to bid
+    private int cardPlayed; //card number player chooses to play for round
+    private int playerScore;    //players total score
+    private int gameStage;  //which state of the game the player is in
+    private int trumpCard;  //value of trump card
+    private int roundNum;
+    private Hashtable<String, Integer> deck = new Hashtable<String, Integer>();   //all the cards in the deck
+    private Hashtable<String, Integer> playerHand = new Hashtable<String, Integer>(); //cards player has in their hand
+
+    public GameState(){
+        playerTurn = 0; //player 0 will go first
+        bidNum = 0;
+        cardPlayed = -1;    //player has no played card yet
+        playerScore = 0;
+        gameStage = 0;      //starts at game state 0: bidding phase
+        trumpCard = -1;     //trump card is not decided yet?
+        roundNum = 1;
+        deck.put("heart zero", 0);    //joker
+        deck.put("heart two", 2);
+        deck.put("heart three", 3);
+        deck.put("heart four", 4);
+        deck.put("heart five", 5);
+        deck.put("heart six", 6);
+        deck.put("heart seven", 7);
+        deck.put("heart eight", 8);
+        deck.put("heart nine", 9);
+        deck.put("heart ten", 10);
+        deck.put("heart eleven", 11); //jack
+        deck.put("heart twelve", 12); //queen
+        deck.put("heart thirteen", 13);   //king
+        deck.put("heart fourteen", 14);   //ace
+        deck.put("heart fifteen", 15);  //wizard
+        deck.put("spade zero", 0);    //joker
+        deck.put("spade two", 2);
+        deck.put("spade three", 3);
+        deck.put("spade four", 4);
+        deck.put("spade five", 5);
+        deck.put("spade six", 6);
+        deck.put("spade seven", 7);
+        deck.put("spade eight", 8);
+        deck.put("spade nine", 9);
+        deck.put("spade ten", 10);
+        deck.put("spade eleven", 11); //jack
+        deck.put("spade twelve", 12); //queen
+        deck.put("spade thirteen", 13);   //king
+        deck.put("spade fourteen", 14);   //ace
+        deck.put("spade fifteen", 15);  //wizard
+        deck.put("diamond zero", 0);    //joker
+        deck.put("diamond two", 2);
+        deck.put("diamond three", 3);
+        deck.put("diamond four", 4);
+        deck.put("diamond five", 5);
+        deck.put("diamond six", 6);
+        deck.put("diamond seven", 7);
+        deck.put("diamond eight", 8);
+        deck.put("diamond nine", 9);
+        deck.put("diamond ten", 10);
+        deck.put("diamond eleven", 11); //jack
+        deck.put("diamond twelve", 12); //queen
+        deck.put("diamond thirteen", 13);   //king
+        deck.put("diamond fourteen", 14);   //ace
+        deck.put("diamond fifteen", 15);  //wizard
+        deck.put("club zero", 0);    //joker
+        deck.put("club two", 2);
+        deck.put("club three", 3);
+        deck.put("club four", 4);
+        deck.put("club five", 5);
+        deck.put("club six", 6);
+        deck.put("club seven", 7);
+        deck.put("club eight", 8);
+        deck.put("club nine", 9);
+        deck.put("club ten", 10);
+        deck.put("club eleven", 11); //jack
+        deck.put("club twelve", 12); //queen
+        deck.put("club thirteen", 13);   //king
+        deck.put("club fourteen", 14);   //ace
+        deck.put("club fifteen", 15);  //wizard
+
+        dealDeck(deck, playerHand); //gives player a random card to start
+    }
+
+    //deals a card out to a player
+    public void dealDeck(Hashtable deck, Hashtable playerHand){
+        Random random = new Random();
+        List<String> shuffleDeck = new ArrayList<String>(deck.keySet());
+        String randomKey = shuffleDeck.get(random.nextInt(shuffleDeck.size()));
+        String value = (String) deck.get(randomKey);
+        Integer myValue = (Integer) deck.get(randomKey);
+        deck.get(value);
+        playerHand.put(value, myValue);
+        deck.remove(value, myValue);
+    }
+
+}
